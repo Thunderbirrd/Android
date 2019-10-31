5516e62bfbec04180cead487318813ba5ccbf0b7
Artem <artemis.gorb.14@gmail.com>
2019-10-31 15:59:04 +0300

    View

---
+++ app/src/main/java/com/example/android/MyView.java
@@ -0,0 +1,16 @@
+package com.example.android;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+public class MyView extends View {
+    public MyView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+}
