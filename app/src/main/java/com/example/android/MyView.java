f9e9bcb7a3a8a7dc6f38d070c25eefb244b7fd5b
Artem <artemis.gorb.14@gmail.com>
2019-11-01 13:40:05 +0300

    1.7.7 Узор

--- app/src/main/java/com/example/android/MyView.java
+++ app/src/main/java/com/example/android/MyView.java
@@ -15,12 +15,20 @@ public class MyView extends View {
         Paint paint = new Paint();
         paint.setStrokeWidth(10);
 
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        canvas.drawLine(100, 10, 100, 800, paint);
+        paint.setColor(Color.MAGENTA);
+        canvas.drawLine(0, 500, this.getWidth(), 600, paint);
+        paint.setColor(Color.GREEN);
+        canvas.drawLine(400, 10, 400, 900, paint);
+        paint.setColor(Color.BLUE);
+        paint.setStrokeWidth(20);
+        canvas.drawLine(500, 10, 500, 800, paint);
         paint.setColor(Color.CYAN);
-        int y = 0;
-        while(y < getHeight() - 20){
-            canvas.drawLine(0, y, this.getWidth(), y + 35, paint);
-            y += 45;
-        }
+        canvas.drawRect(800, 800, getHeight(), getHeight(), paint);
+        paint.setColor(Color.YELLOW);
+        canvas.drawCircle(600, 200, 150, paint);
     }
 
 }
