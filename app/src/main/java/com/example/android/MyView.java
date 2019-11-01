92bc4255a7b57378b9e808df24db9762c91080bf
Artem <artemis.gorb.14@gmail.com>
2019-11-01 14:28:23 +0300

    1.8.7 Много шариков

--- app/src/main/java/com/example/android/MyView.java
+++ app/src/main/java/com/example/android/MyView.java
@@ -5,22 +5,37 @@ import android.graphics.*;
 import android.view.View;
 
 public class MyView extends View {
+    int N = 10;
+    float[] x  = new float[N];
+    float[] y  = new float[N];
+    float[] vx = new float[N];
+    float[] vy = new float[N];
+    boolean started;
     public MyView(Context context) {
         super(context);
+        if(!started) {
+            for (int i = 0; i < N; i++) {
+                x[i] = (float) (Math.random() * 500);
+                y[i] = (float) (Math.random() * 500);
+                vx[i] = (float) (Math.random() * 6 - 3);
+                vy[i] = (float) (Math.random() * 6 - 3);
+            }
+            started = true;
+        }
     }
-
 
     Paint paint = new Paint();
-    float x = 50;
-    long lastTime = System.currentTimeMillis();
+    //long lastTime = System.currentTimeMillis();
     @Override
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        canvas.drawCircle(x, 500, 30, paint);
-        long nowTime = System.currentTimeMillis();
-        x += 0.1f * (nowTime - lastTime);
-        lastTime = nowTime;
+        paint.setColor(Color.RED);
+        for (int i = 0; i < N; i++) {
+            canvas.drawCircle(x[i], y[i], 25, paint);
+        }
+        for (int i = 0; i < N; i++) {
+            x[i] += vx[i];
+            y[i] += vy[i];
+        }
         invalidate();
     }
-
 }
