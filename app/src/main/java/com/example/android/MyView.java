a93c3a062936d825c1a314e7560fb00dc1cd5279
Artem <artemis.gorb.14@gmail.com>
2019-11-01 13:39:43 +0300

    1.7.7 Diags

--- app/src/main/java/com/example/android/MyView.java
+++ app/src/main/java/com/example/android/MyView.java
@@ -15,10 +15,12 @@ public class MyView extends View {
         Paint paint = new Paint();
         paint.setStrokeWidth(10);
 
-
         paint.setColor(Color.CYAN);
-        paint.setStyle(Paint.Style.FILL);
-        canvas.drawRect(200, 200, 800, 800, paint);
+        int y = 0;
+        while(y < getHeight() - 20){
+            canvas.drawLine(0, y, this.getWidth(), y + 35, paint);
+            y += 45;
+        }
     }
 
 }
